4d92f30fb481cd0bdbb4fba9fabe92cd590fed3e
ranasarkar <ranasarkar739@gmail.com>
2024-06-08 01:22:28 +0200

    server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,70 +2,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
-
-     try {
-       serverSocket = new ServerSocket(4221);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-         InputStream inputStream = clientSocket.getInputStream();
-         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-         String firstLine = reader.readLine();
-         String line;
-         String userAgent = null;
-         while((line = reader.readLine())!=null){
-             if(line.startsWith("User-Agent:")){
-                 String[] userAgentTokens = line.split(":");
-                 userAgent = userAgentTokens[1];
-                 System.out.println(userAgent);
-                 break;
-             }
-         }
-
-         if(firstLine==null || firstLine.isEmpty()){
-             clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-             return;
-         }
-         String[] tokens = firstLine.split(" ");
-         String endpoint = tokens[1];
-         System.out.println(endpoint);
-         if(endpoint.equalsIgnoreCase("/user-agent")){
-             if(userAgent!=null){
-                 String output = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",userAgent.trim().length(),userAgent.trim());
-                 clientSocket.getOutputStream().write(output.getBytes());
-                 return;
-             }
-         }
-         if(endpoint.equalsIgnoreCase("/")){
-             clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-             return;
-         }
-         String[] endpointTokens = endpoint.split("/");
-         if(endpointTokens[1].equalsIgnoreCase("echo")){
-             System.out.println("Here");
-             String output1 = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",endpointTokens[2].length(),endpointTokens[2]);
-             System.out.println(output1);
-             clientSocket.getOutputStream().write(output1.getBytes());
-         }else{
-             clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-         }
-
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible
+        // when running tests.
+        System.out.println("Logs from your program will appear here!");
+        // Uncomment this block to pass the first stage
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        try {
+            serverSocket = new ServerSocket(4221);
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept(); // Wait for connection from client.
+            InputStream input = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String line = reader.readLine();
+            String[] HttpRequest = line.split(" ");
+            OutputStream output = clientSocket.getOutputStream();
+            String[] str = HttpRequest[1].split("/");
+            // System.out.println(HttpRequest[1]);
+            if (HttpRequest[1].equals("/")) {
+                System.out.println("version");
+                String response = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: text/plain\r\n"
+                        + "Content-Length: 0\r\n\r\n";
+                output.write(response.getBytes());
+            } else if (str[1].equals("user-agent")) {
+                reader.readLine();
+                String useragent = reader.readLine().split("\\s+")[1];
+                String reply = String.format(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s\r\n",
+                        useragent.length(), useragent);
+                output.write(reply.getBytes());
+            } else if ((str.length > 2 && str[1].equals("echo"))) {
+                String responsebody = str[2];
+                String finalstr = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: text/plain\r\n"
+                        + "Content-Length: " + responsebody.length() +
+                        "\r\n\r\n" + responsebody;
+                output.write(finalstr.getBytes());
+            } else {
+                output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+            }
+            output.flush();
+            System.out.println("accepted new connection");
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
+
