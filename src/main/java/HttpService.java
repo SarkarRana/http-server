16a295061a8f4eeb66084d111fc64ec9547e0b45
ranasarkar <ranasarkar739@gmail.com>
2024-06-09 00:58:00 +0200

    server

--- src/main/java/HttpService.java
+++ src/main/java/HttpService.java
@@ -29,17 +29,29 @@ public class HttpService implements Runnable{
                 return;
             }
             String[] HttpRequest = line.split(" ");
+            System.out.println(String.join(",",HttpRequest));
+            String headerLine;
+            StringBuffer headers = new StringBuffer();
+            while((headerLine= reader.readLine())!=null && !headerLine.isEmpty()){
+                headers.append(headerLine);
+            }
+            String requestBody = reader.readLine();
+            System.out.println("body:: "+requestBody);
 
 
             String[] str = HttpRequest[1].split("/");
             // System.out.println(HttpRequest[1]);
             if (HttpRequest[1].equals("/")) {
+                System.out.println("Here1:::::");
+
                 System.out.println("version");
                 String response = "HTTP/1.1 200 OK\r\n"
                         + "Content-Type: text/plain\r\n"
                         + "Content-Length: 0\r\n\r\n";
                 output.write(response.getBytes());
             } else if (str[1].equals("user-agent")) {
+                System.out.println("Here1:::::");
+
                 reader.readLine();
                 String useragent = reader.readLine().split("\\s+")[1];
                 String reply = String.format(
@@ -47,13 +59,17 @@ public class HttpService implements Runnable{
                         useragent.length(), useragent);
                 output.write(reply.getBytes());
             } else if ((str.length > 2 && str[1].equals("echo"))) {
+                System.out.println("Here1:::::");
+
                 String responsebody = str[2];
                 String finalstr = "HTTP/1.1 200 OK\r\n"
                         + "Content-Type: text/plain\r\n"
                         + "Content-Length: " + responsebody.length() +
                         "\r\n\r\n" + responsebody;
                 output.write(finalstr.getBytes());
-            } else if ((str.length > 2 && str[1].equals("files"))) {
+            } else if (HttpRequest[0].equalsIgnoreCase("GET") && (str.length > 2 && str[1].equals("files"))) {
+                System.out.println("Here1:::::");
+
                 String filePath = basePath+str[2];
                 File file = new File(filePath);
                 if(file.exists()){
@@ -66,10 +82,32 @@ public class HttpService implements Runnable{
                             "\r\n\r\n" + body;
                     output.write(finalstr.getBytes());
                 }else{
+                    System.out.println("Here1:::::");
                     output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
                 }
 
 
+            }else if(HttpRequest[0].startsWith("POST") && (str.length > 2 && str[1].equals("files"))){
+                System.out.println("Here:::::");
+                String fileName = str[2];
+                try {
+                    String fullFilePath = basePath + fileName + ".txt";
+                    System.out.println("Filename " + fullFilePath);
+                    File outputFile = new File(basePath,fileName);
+                    if (outputFile.exists()) {
+                        String finalstr = "HTTP/1.1 201 Created\r\n" + "Content-Type: application/octet-stream\r\n"
+                                + "Content-Length: " + requestBody.length() +
+                                "\r\n\r\n" + requestBody;
+                        System.out.println("finalstring:: " + finalstr);
+                        output.write(finalstr.getBytes());
+                        System.out.println("output sent");
+                    }
+
+
+
+                }catch (Exception ex){
+                    System.out.println("Exception occurred:: "+ex.getMessage());
+                }
             }else {
                 output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
             }
